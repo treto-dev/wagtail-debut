3603ef5069b0d52a7a00366567018baa8befd241
Fabien Le Frapper <contact@fabienlefrapper.me>
2021-02-07 01:51:34 +0100

    Fix: static generation badly exported in case of grapple setup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -7,11 +7,11 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 DOCKER_DIR = os.path.join(PROJECT_DIRECTORY, "docker", "config")
 TO_REMOVE = []
 TO_RENAME = []
-
 GRAPPLE_FILES = [
+    "frontend/pages/[...path].grapple.tsx",
     "frontend/pages/_app.grapple.tsx",
     "frontend/pages/_preview.grapple.tsx",
-    "frontend/pages/[...path].grapple.tsx",
+    "frontend/pages/index.grapple.tsx",
 ]
 
 print(os.getcwd())  # prints /absolute/path/to/{{cookiecutter.project_slug}}
@@ -34,15 +34,29 @@ use_grapple = "{{cookiecutter.use_grapple}}" == "yes"
 use_circle_ci = "{{cookiecutter.use_circle_ci}}" == "yes"
 
 if not use_decoupled_frontend:
+    """
+    In case the project does not use a decoupled frontend, no static frontend is usefuled.
+    Also: no need to have the specific nextjs app.
+    """
     TO_REMOVE += ["frontend", "wagtail/nextjs"]
 else:
     if use_grapple:
+        """
+        A set of grapple-specific files has been created, with a .grapple.tsx extension.
+        In case the project uses Grapple, they should be used instead of the original ones.
+        """
         for file in GRAPPLE_FILES:
             TO_RENAME.append((file, file.replace(".grapple", "")))
     else:
+        """
+        In case the project do not make use of Grapple, let's not keep these files.
+        """
         TO_REMOVE += GRAPPLE_FILES
 
 if not use_circle_ci:
+    """
+    Everything circle CI related can be removed.
+    """
     TO_REMOVE += [".circleci", ".ciignore"]
 
 shutil.copyfile(
@@ -50,6 +64,10 @@ shutil.copyfile(
     os.path.join(DOCKER_DIR, "python.env"),
 )
 
+
+"""
+Now that all non-required files have been set, let's rename or remove these.
+"""
 for path in TO_REMOVE:
     remove(path)
 
--- {{cookiecutter.project_name}}/frontend/pages/index.ts
+++ {{cookiecutter.project_name}}/frontend/pages/index.ts
@@ -1,5 +1,1 @@
-// For SSR
 export { default, getServerSideProps } from 'pages/[...path]';
-
-// For SSG
-// export { default, getStaticProps } from './[...path]';
